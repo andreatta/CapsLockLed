b6d4bfff48620c48d5d31a0d4d2f9f06135d1039
Cyril Andreatta <cyril@andreatta.ch>
2017-07-10 18:01:08 +0200

    Add function to find correct path to capslock-LED.
    - If program is called without parameter, LED is toggled

--- capslockled.c
+++ capslockled.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <string.h>
 
-#define EXIT_SUCCESS (0)
-#define EXIT_FAILURE (1)
+#define EXIT_SUCCESS 0
+#define EXIT_FAILED_OPEN 1
+#define EXIT_FAILED_WRITE 2
+#define EXIT_WRONG_PARAM 3
+
+int get_path(char *path);
 
 /**
  * Function to set LED on CapsLock Button
@@ -21,28 +27,69 @@
  * \return 0 on success
  * \return 1 on failure
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
     FILE *led;
     int written = 0;
     unsigned char state = '0';
-    const char *ledpath="/sys/class/leds/input6::capslock/brightness";
+    char ledpath[60] = {0};
+    //const char *ledpath="/sys/class/leds/input6::capslock/brightness";
 
-    led = fopen(ledpath, "w");
+    get_path(ledpath);
 
+    led = fopen(ledpath, "r+");
     if (led == NULL)
-        return EXIT_FAILURE;
+        return -EXIT_FAILED_OPEN;
 
-    if (argc >= 2) {
+    /* If no param is given toggle LED */
+    if (argc == 1) {
+        fread(&state, 1 , 1, led);
+        state = (state == '0' ? '1' : '0');
+    } else if (argc >= 2) {
         if (*argv[1] == '1')
             state = '1';
-
-        written = fwrite(&state, 1, 1, led);
-
-        if (written != 1)
-            return EXIT_FAILURE;
+    } else {
+        return -EXIT_WRONG_PARAM;
     }
+
+    written = fwrite(&state, 1, 1, led);
+    if (written != 1)
+        return -EXIT_FAILED_WRITE;
 
     fclose(led);
     return EXIT_SUCCESS;
+}
+
+/**
+ * Get correct path to LED
+ * On my system (Dell XPS15  9560), the input number happened to change
+ * from time to time.
+ */
+int get_path(char *path)
+{
+    static char ledpath[60] = "/sys/class/leds/";
+    int ledlen = 0;
+    DIR *d = opendir(ledpath);
+    struct dirent *dir;
+
+    if (d == NULL)
+        return -1;
+
+    ledlen = strlen(ledpath);
+
+    while ((dir = readdir(d)) != NULL) {
+        if (dir->d_type != DT_DIR) {
+            if (strstr(dir->d_name, "capslock") != NULL) {
+                int len = strlen(dir->d_name);
+                strncat(&ledpath[ledlen], dir->d_name, len);
+                strncat(&ledpath[ledlen + len], "/brightness", 12);
+            }
+        }
+    }
+    closedir(d);
+
+    ledlen = strlen(ledpath);
+    memcpy(path, ledpath, ledlen);
+
+    return ledlen;
 }
